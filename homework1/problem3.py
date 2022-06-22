b8c6693cc3aedf01ebdc1aeb6aa438d894fe20b0
carolyn-brodie <carolyn.brodie@simpson.edu>
2022-06-22 15:07:27 -0500

    Initial commit

---
+++ homework1/problem3.py
@@ -0,0 +1,22 @@
+# Predict what each of the does.
+# a.	len("Hello")
+# b.	len("2")
+# c.	len("ten") + len(" ") + len("123")
+# d.	len(2)
+# e.	len("")
+# First make a prediction and modify the appropriate print
+# statement with your prediction as shown in
+# Part a (part a is completed as an example)
+print('prediction for len("Hello")', 5)
+print('prediction for len("2")')
+print('prediction for len("ten") + len(" ") + len("123")')
+print('prediction for len(2)')
+print('prediction for len("")')
+
+# Then print out each to see if your prediction was correct
+# Again part a is completed as an example
+print(len("Hello"))
+
+# If any of your predictions were different then the result,
+# explain the difference or ask me a question in comments
+
