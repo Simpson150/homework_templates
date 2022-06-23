6af2dc3c73f7e33dda897291c8e8c96a39b053b7
carolyn-brodie <carolyn.brodie@simpson.edu>
2022-06-23 10:44:54 -0500

    homework1

---
+++ homework1/problem2.py
@@ -0,0 +1,23 @@
+# Predict what each of the does.
+# a.	7 / 2
+# b.	7 // 2
+# c.	2 / 0
+# d.	1 / (3 // 2)
+# e.	1 / 3 // 2
+# First make a prediction and modify the appropriate print
+# statement with your prediction as shown in
+# Part a (part a is completed as an example)
+print("prediction for 7 / 2", 3.5)
+print("prediction for 7 // 2")
+print("prediction for 2 / 0")
+print("prediction for 1 / (3 // 2)")
+print("prediction for 1 / 3 // 2")
+
+# Then print out each to see if your prediction was correct
+# Again part a is completed as an example
+print(7 / 2)
+
+# If any of your predictions were different then the result,
+# explain the difference or ask me a question in comments
+
+
